0ebcc1e8e6ce3f28dfe6fb0671520872fb498944
Sevak Mardirosian <smardirosian@cosine.nl>
2016-08-07 17:16:05 +0200

    Dynamic Array

--- DataStructures/Dynamic_Array/Solution.java
+++ DataStructures/Dynamic_Array/Solution.java
@@ -1,5 +1,37 @@
 package Dynamic_Array;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Solution {
-	
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int q = scanner.nextInt();
+		int lastAns = 0;
+		List<List> seqList = new ArrayList();
+
+		for (int i = 0; i < n; i++) {
+			seqList.add(new ArrayList<Integer>());
+		}
+
+		for (int i = 0; i < q; i++) {
+			int t = scanner.nextInt();
+			int x = scanner.nextInt();
+			int y = scanner.nextInt();
+
+			List<Integer> seq = seqList.get((x ^ lastAns) % n);
+			if (t == 1) {
+				seq.add(y);
+			} else {
+				lastAns = seq.get(y % (seq.size()));
+				System.out.println(lastAns);
+			}
+		}
+
+		scanner.close();
+	}
 }
