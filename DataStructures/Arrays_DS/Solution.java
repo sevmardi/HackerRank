55e054e43f1896be2795f05383892d8e325dacc7
Sevak .M <s1079770@student.hsleiden.nl>
2016-08-27 22:18:09 +0200

    small fix

--- DataStructures/Arrays_DS/Solution.java
+++ DataStructures/Arrays_DS/Solution.java
@@ -1,6 +1,5 @@
 package Arrays_DS;
 
-<<<<<<< 950b24cfcb79d47457afcd1c85c9fe1474cd0a6a
 import java.util.Scanner;
 
 public class Solution {
@@ -18,15 +17,5 @@ public class Solution {
 		for (int j = N - 1; j >= 0; j--) {
 			System.out.println(arr[j] + " ");
 		}
-
-=======
-public class Solution {
-	
-	public static void main(String[] args) {
-		
-		
-		
-		
->>>>>>> e3b300bf48955366198ab21d67635225e383d614
 	}
 }
